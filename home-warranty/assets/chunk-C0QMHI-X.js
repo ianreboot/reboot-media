1ef4140bfeefacf8651ce0dae3290e18942cdb85
$(grep GIT_USER_NAME /home/.env | cut -d'=' -f2) <$(grep GIT_USER_EMAIL /home/.env | cut -d'=' -f2)>
2025-09-09 17:20:42 +0000

    Add home-warranty assets for proper deployment

---
+++ home-warranty/assets/chunk-C0QMHI-X.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-BbdgBc_e.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("MapPin",[["path",{d:"M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0",key:"1r0f0z"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("MessageCircle",[["path",{d:"M7.9 20A9 9 0 1 0 4 16.1L2 22Z",key:"vv11sd"}]]);export{r as M,c as a};
